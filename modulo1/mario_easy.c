f0ebc83b2e5918fcd462dbacc972912a035f81f3
Wesley Benício <wesleybenicio4@gmail.com>
2022-05-31 19:43:18 -0300

    Modulo 1 - Mario Easy

---
+++ modulo1/mario_easy.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+    // Declaring Variables
+    int lines = 0;
+    char* column;
+
+    // Get Number of Lines
+    printf("Digite a Altura: ");
+    scanf("%i",&lines);
+
+    // Dynamic Variable
+    column = (char*) malloc ((lines+1)*sizeof(char));
+
+    // Defining Default Values
+    for (int i = 0; i < lines; i++) column[i] = ' ';
+
+    column[lines] = '\0';
+
+    for(int v = lines;v>0;v--){
+        column[v-1] = '#';
+        printf("%s\n",column);
+    }
+
+    free(column);
+}
